ee8bc346888aabedf23c04433470b3f0b85e2c5e
ondirazamatov <ahdiir@gmail.com>
2022-03-15 23:59:13 +0600

    some more example

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def demp_zip():
    for a, b in zip(range(5,10), range(10,20,2)):
         print(a, b)
+def demo_walrus():
+    a =['spam','eggs']
+    if a_len := len(a):
+        print("a>0", a_len)
+    else:
+        print(a)
+
 def main():
     demp_zip()
 if __name__ == '__main__':
